23c66a8a86a465666582f6394587e38b02751db6
SourSolution <peterhaugsted@hotmail.com>
2022-04-30 19:47:00 +0200

    Datepicker

--- src/main/java/EditShiftsController.java
+++ src/main/java/EditShiftsController.java
@@ -1,9 +1,22 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.DateCell;
+import javafx.scene.control.DatePicker;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Callback;
 
+import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
-public class EditShiftsController implements AppContact{
+public class EditShiftsController implements AppContact {
 
     @FXML
     private APPHANDLER app;
@@ -20,7 +33,39 @@ public class EditShiftsController implements AppContact{
     }
 
 
-    public void GoBackMenuR(MouseEvent mouseEvent) throws IOException{
+    public void GoBackMenuR(MouseEvent mouseEvent) throws IOException {
         app.GoBackToRmenu();
+    }
+
+    private void datepickerSetDate() throws ParseException {
+
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        String dateInString = "2022-04-30";
+        Date date = formatter.parse(dateInString);
+
+        Instant instant = date.toInstant();
+        LocalDate localDate = instant.atZone(ZoneId.systemDefault()).toLocalDate();
+
+        ObservableList<LocalDate> selectedDates = FXCollections.observableArrayList();
+        selectedDates.addAll(localDate);
+
+        DatePicker datePicker_1 = new DatePicker();
+        datePicker_1.setOnAction(event -> selectedDates.add(datePicker_1.getValue()));
+
+        datePicker_1.setDayCellFactory(new Callback<DatePicker, DateCell>() {
+            @Override
+            public DateCell call(DatePicker param) {
+                return new DateCell() {
+                    @Override
+                    public void updateItem(LocalDate item, boolean empty) {
+                        super.updateItem(item, empty);
+                        boolean alreadySelected = selectedDates.contains(item);
+                        setDisable(alreadySelected);
+                        setStyle(alreadySelected ? "-fx-background-color: lightblue;" : "");
+                    }
+                };
+            }
+        });
     }
 }
