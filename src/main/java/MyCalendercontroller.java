fc942fd74384d38056ee94e7df6e34c0835f5b99
SourSolution <peterhaugsted@hotmail.com>
2022-05-03 02:22:54 +0200

    Load save vbox

--- src/main/java/MyCalendercontroller.java
+++ src/main/java/MyCalendercontroller.java
@@ -1,10 +1,14 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
 
+import javax.swing.*;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,6 +24,42 @@ public class MyCalendercontroller implements AppContact {
     @FXML
     private Label myLabel;
 
+    @FXML
+    VBox vbMenu;
+
+    FileChooser fileChooser = new FileChooser();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        fileChooser.setInitialDirectory(new File("C:\\temp"));
+    }
+
+    @FXML
+    private void handleSaveClicked(ActionEvent event){
+        Window stage = vbMenu.getScene().getWindow();
+        fileChooser.setTitle("Save Dialog");
+        fileChooser.setInitialFileName("mysave");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("text file", "*.txt"), new FileChooser.ExtensionFilter("pdf", "*.pdf"));
+
+        try{
+            File file = fileChooser.showSaveDialog(stage);
+            fileChooser.setInitialDirectory(file.getParentFile());
+            //TODO save
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    private void handleLoadClicked(ActionEvent event){
+        Window stage = vbMenu.getScene().getWindow();
+        fileChooser.setTitle("Load Dialog");
+
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("text file", "*txt", "*.docx", "*.doc"),
+                new FileChooser.ExtensionFilter("pdf", "*.pdf"),
+                new FileChooser.ExtensionFilter("images", "*.jpg", "*gif"));
+    }
 
     private String[] time = {"8:00","10:00","12:00","14:00","16:00","18:00","20:00"};
 
