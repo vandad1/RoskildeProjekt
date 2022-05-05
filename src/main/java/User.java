212074bd880f1690d10dccd8de64ce42138dcef2
emilozel <emil234@hotmail.dk>
2022-05-05 14:27:29 +0200

    hola

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,122 +1,45 @@
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.GregorianCalendar;
-
-public class User implements Serializable {
+public class User {
     private String name;
-    private String eMail;
-    private String tlfNr;
-    private String address;
-    private String password;
-    private boolean responsible;
-    private ArrayList<Shift> shifts;
+    private String task;
+    private String date;
+    private String time;
 
-    public boolean isResponsible() {
-        return responsible;
-    }
-
-    public void setResponsible(boolean responsible) {
-        this.responsible = responsible;
-    }
-
-    public User(String name, String eMail, String tlfNr, String address, String password) {
+    public User(String name, String task, String date, String time) {
         this.name = name;
-        this.eMail = eMail;
-        this.tlfNr = tlfNr;
-        this.address = address;
-        this.password = password;
-        responsible = false;
-        shifts = new ArrayList<>();
+        this.task = task;
+        this.date = date;
+        this.time = time;
     }
 
-    public String getName() {
+    public String getName(){
         return name;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public String geteMail() {
-        return eMail;
+    public void setTask(String task) {
+        this.task = task;
     }
 
-    public void seteMail(String eMail) {
-        this.eMail = eMail;
+    public void setDate(String date) {
+        this.date = date;
     }
 
-    public String getTlfNr() {
-        return tlfNr;
+    public void setTime(String time) {
+        this.time = time;
     }
-
-    public void setTlfNr(String tlfNr) {
-        this.tlfNr = tlfNr;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public ArrayList<Shift> getShifts() {
-        return shifts;
-    }
-
-    public void setShifts(ArrayList<Shift> shifts) {
-        this.shifts = shifts;
-    }
-
-    public Shift getNextShift() {
-        Shift nextShift = null;
-        long minDelta = Long.MAX_VALUE;
-        for (int i = 0; i < shifts.size(); i++) {
-            long delta = shifts.get(i).millsFromNow();
-            if (delta < minDelta && delta != -1) {
-                minDelta = delta;
-                nextShift = shifts.get(i);
-            }
-        }
-        return nextShift;
-    }
-
-    public ArrayList<Shift> getNext3Shifts(){
-        ArrayList<Shift> moreShifts = new ArrayList<>();
-        try {
-            int n = 3;
-            for (int i = 0; i < n; i++) {
-                if (shifts.get(i) != null) {
-                    if (shifts.get(i).millsFromNow() != -1){
-                        moreShifts.add(shifts.get(i));
-                    } else {
-                        n++;
-                    }
-                } else {
-                    break;
-                }
-            }
-        } catch (Exception e){
-
-        }
-        return moreShifts;
-    }
-
-    public boolean addShift(Shift shift){
-        shifts.add(shift);
-        Collections.sort(shifts, new shiftComparable());
-        return Database.saveVToFile();
-    }
-
-
 }
