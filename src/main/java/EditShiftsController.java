abdf3a8d933996878b092a12c6915727e23234b2
emilozel <emil234@hotmail.dk>
2022-05-05 14:28:26 +0200

    hola

--- src/main/java/EditShiftsController.java
+++ src/main/java/EditShiftsController.java
@@ -4,7 +4,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.MouseEvent;
 
 
@@ -12,18 +14,17 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class EditShiftsController implements AppContact {
+public class EditShiftsController implements Initializable, AppContact {
 
-    @Override
-    public void setApp(APPHANDLER app) {
-            this.app = app;
-        }
 
     @FXML
     public APPHANDLER app;
 
     @FXML
     public TableView<User> table;
+
+    @FXML
+    public TableColumn<User, String> name;
 
     @FXML
     public TableColumn<User, String> task;
@@ -46,22 +47,36 @@ public class EditShiftsController implements AppContact {
 
 
     ObservableList<User> List = FXCollections.observableArrayList(
-            new User("Goere rent", "05/03/2022", "15:00 - 19:00"),
-            new User("Rens", "05/03/2022", "15:00 - 20:00"),
-            new User("Hotdog service", "05/03/2022", "20:00 - 24:00")
+            new User("Peter", "Goere rent", "05/03/2022", "15:00 - 19:00"),
+            new User("Vandad", "Diktere opgaver", "5/4/2022", "15:00 - 20:00"),
+            new User("Emil", "Ornlig syg kodning", "5/4/2022", "17:00 - 21:00")
     );
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        name.setCellValueFactory(new PropertyValueFactory<User, String>("name"));
+        task.setCellValueFactory(new PropertyValueFactory<User, String>("task"));
+        date.setCellValueFactory(new PropertyValueFactory<User, String>("date"));
+        time.setCellValueFactory(new PropertyValueFactory<User, String>("time"));
+        table.setEditable(true);
+        name.setEditable(true);
+        task.setEditable(true);
+        date.setEditable(true);
+        time.setEditable(true);
+        name.setCellFactory(TextFieldTableCell.forTableColumn());
+        task.setCellFactory(TextFieldTableCell.forTableColumn());
+        date.setCellFactory(TextFieldTableCell.forTableColumn());
+        time.setCellFactory(TextFieldTableCell.forTableColumn());
 
-    public void List(APPHANDLER app, ResourceBundle resourceBundle) {
-        task.setCellValueFactory(new PropertyValueFactory<User, String>("getTask"));
-        date.setCellValueFactory(new PropertyValueFactory<User, String>("getDate"));
-        time.setCellValueFactory(new PropertyValueFactory<User, String>("getTime"));
 
         table.setItems(List);
     }
 
-
+    @Override
+    public void setApp(APPHANDLER app) {
+        this.app = app;
     }
+}
 
 
 
